db3578750f069372ae19300ba607ae107f047944
Gathua Kiragu <kiragugathua@gmail.com>
2017-07-26 06:04:12 +0300

    Created a hero object Assigned its properties with values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+// Creating a Hero object
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
 })
+
+
 export class AppComponent {
-  title = 'To Reece Technologies';
+  title = 'Tour of Heroes';
+
+// Assigning hero object with values
+  hero: Hero = {
+  id: 1,
+  name: 'Windstormyy'
+  }
 }
